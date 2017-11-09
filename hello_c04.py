a6b87abf9936c7ece1ecf810744c5699084adb60
dcr1586@inbox.lv <getutnet@gmail.com>
2017-11-09 16:00:58 +0200

    20171019_16_10

---
+++ hello_c04.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+print "Hello world!"
+'''
+
+#https://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3. - 7.
+
+# example 3
+print "Here \byhe \bare \bbackspaced."
+
+# example 4
+print "Here \nthe \nspaces \nare \nnewlined."
+
+# example 5
+
+print "Here \tis \tnew \texample"
+# example 6
+print "Man \vgarso \vvariti \vkartupeli"
+
+# example 7 
+print "Here the \rcontent before this is removed."
+
+# example 8
+print "Here the content after "," is not printed , \c This is not printed on console"
